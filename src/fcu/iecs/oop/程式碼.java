158602b17cb1d2c0b65358456abc0616cbd60a2c
user <user@DESKTOP-NTCNMKQ>
2016-10-07 02:00:29 +0800

    程式碼

--- src/fcu/iecs/oop/程式碼.java
+++ src/fcu/iecs/oop/程式碼.java
@@ -1,5 +1,11 @@
 package fcu.iecs.oop;
 
-public class µ{¦¡½X {
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics="let her go!let her go!Can't hold her back anymore";
+		System.out.println("let ''her'' go ! let ''her'' go  !Can't hold ''her'' back anymore");
+	}
 
 }
