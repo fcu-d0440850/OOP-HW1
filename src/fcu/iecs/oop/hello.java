ea7acab0f2bbb69015b6dc9fc2f6f56b6abed6e4
user <user@DESKTOP-NTCNMKQ>
2016-10-07 00:27:36 +0800

    第一版程式碼

--- src/fcu/iecs/oop/hello.java
+++ src/fcu/iecs/oop/hello.java
@@ -5,7 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String lyrics="let her go!let her go!Can't hold her back anymore";
-		System.out.println("let ''her''go! let ''her'' go!Can't hold ''her'' back anymore");
+		System.out.println("let ''her'' go ! let ''her'' go  !Can't hold ''her'' back anymore");
 	}
 
 }
