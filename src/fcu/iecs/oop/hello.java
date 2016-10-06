3a398a1a1672ec7ade43be485628f480909066bb
user <user@DESKTOP-NTCNMKQ>
2016-10-07 00:10:20 +0800

    2u4u c;6t/6g4a83
    Signed-off-by: user <user@DESKTOP-NTCNMKQ>

---
+++ src/fcu/iecs/oop/hello.java
@@ -0,0 +1,11 @@
+package fcu.iecs.oop;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String lyrics="let her go!let her go!Can't hold her back anymore";
+		System.out.println("let ''her''go! let ''her'' go!Can't hold ''her'' back anymore");
+	}
+
+}
